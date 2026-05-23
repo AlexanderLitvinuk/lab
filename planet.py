19a33418174148fad2b7443cda12cb3021b0795a
AlexanderLitvinuk <kabankabanovic24@gmail.com>
2026-05-23 21:34:51 +0300

    Update planet.py
    описание

--- planet.py
+++ planet.py
@@ -4,84 +4,109 @@ types_list = ("каменная","газовый гигант", "ледяной 
 
 class Planet:
     def __init__(self, name, radius, mass, length, typep, idp):
-        self.set_id(idp)
-        self.set_name(name)
-        self.set_radius(radius)
-        self.set_mass(mass)
-        self.set_length(length)
-        self.set_type(typep)
-        self.set_id(idp)
+        self.idp = idp
+        self.name = name
+        self.radius = radius
+        self.mass = mass
+        self.length = length
+        self.typep = typep
+        print("Создана планета " + self.name + " с ID " + str(self.idp))
         
-        
+    @property
+    def name(self):
+        return self._name
     
-    def set_name(self, name):
-        self.name = to_word(name)
+    @property
+    def radius(self):
+        return self._radius
+    
+    @property
+    def mass(self):
+        return self._mass
+    
+    @property
+    def length(self):
+        return self._length
+    
+    @property
+    def typep(self):
+        return self._typep
+    
+    @property
+    def idp(self):
+        return self._idp
         
-    def set_radius(self, radius):
+    @name.setter
+    def name(self, name):
+        self._name = to_word(name)
+    
+    @radius.setter
+    def radius(self, radius):
         _n = to_number(radius)
         if _n > 0:
-            self.radius = _n
+            self._radius = _n
         else:
-            self.radius = 0
-            
-    def set_mass(self, mass):
+            self._radius = 0
+    
+    @mass.setter
+    def mass(self, mass):
         _n = to_number(mass)
         if _n > 0:
-            self.mass = _n
+            self._mass = _n
         else:
-            self.mass = 0
-            
-    def set_length(self, length):
+            self._mass = 0
+    
+    @length.setter
+    def length(self, length):
         _n = to_number(length)
         if _n > 0:
-            self.length = _n
+            self._length = _n
         else:
-            self.length = 0
-    def set_type(self, typep):
-        _s = to_word(typep.lower())
-        if _s in types_list:
-            self.typep = _s
+            self._length = 0
+    
+    @typep.setter
+    def typep(self, typep):
+        _s = to_word(typep)
+        if _s.lower() in types_list:
+            self._typep = _s
         else:
-            self.typep = "не установлено"
-            
-    def set_id(self, idp):
+            self._typep = "не установлено"
+    
+    @idp.setter 
+    def idp(self, idp):
         _n = to_int(idp)
-        if idp > 0:
-            self.idp = _n
+        if _n > 0:
+            self._idp = _n
         else:
-            self.idp = 0
-            
-            
-    
-    def get_name(self):
-        return self.name
+            self._idp = 0
 
-    def get_radius(self):
-        return self.radius
-    
-    def get_mass(self):
-        return self.mass
-    
-    def get_length(self):
-        return self.length
-    
-    def get_type(self):
-        return self.typep
-    
-    def get_id(self):
-        return self.idp
-    
-    
+    def __repr__(self):
+        s = []
+        s.append(str(self.idp))
+        s.append(self.name)
+        s.append(str(self.radius))
+        s.append(str(self.mass))
+        s.append(str(self.length))
+        s.append(str(self.typep))
+        full_s = ";".join(s)
+        return full_s
+
     def __str__(self):
         s = []
         s.append("ID: " + str(self.idp))
         s.append("Название: " + self.name)
-        s.append("Радиус: " + str(self.radius) + " м^6")
-        s.append("Масса: " + str(self.mass) + " кг^6")
-        s.append("Расстояние до Солнца: " + str(self.length) + "м^6")
+        s.append("Радиус: " + str(self.radius) + " км")
+        s.append("Масса: " + str(self.mass) + " млн тонн")
+        s.append("Расстояние до Солнца: " + str(self.length) + " млн км")
         s.append("Тип: " + str(self.typep))
         full_s = "\n".join(s)
         return full_s
-
-nig = Planet("[", "1fas12", "dsa12", "dadsajj3223,32.", "23", 13.2)
-print(nig)
+    
+    def __lt__(self, other): # <
+        return self.length < other.length
+    
+    def __eq__(self, other):
+        return self.name == other.name
+    
+    def __del__(self):
+        print("Удалена планета " + self.name + " с ID " + str(self.idp))
